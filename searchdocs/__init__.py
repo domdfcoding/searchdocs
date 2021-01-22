89db7441a37db0f64577dbccf856674a2d28d68f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-22 13:55:33 +0000

    Bump version v0.1.0 -> v0.1.1

--- searchdocs/__init__.py
+++ searchdocs/__init__.py
@@ -50,7 +50,7 @@ __all__ = [
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 #: Directory in which cached files are stored.
